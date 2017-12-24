245180b8bdb39821d9335bd33e1cdce7cbad4edb
Jonathan Kaufman <jkaufman@squarespace.com>
2017-12-24 15:13:33 -0500

    retry promise logic and parallelize requests

--- spotify.js
+++ spotify.js
@@ -1,5 +1,19 @@
 const SpotifyWebApi = require('spotify-web-api-node');
 const ProgressBar = require('progress');
+const Promise = require('bluebird');
+const { flatten } = require('lodash');
+
+const MAX_RETRIES = 5;
+const RETRY_INTERVAL = 10 * 1000;
+
+const barOpts = total => ({
+  complete: '=',
+  incomplete: '-',
+  width: 20,
+  total
+});
+
+const asyncTimeout = (duration) => new Promise((resolve) => setTimeout(resolve, duration))
 
 const spotify = new SpotifyWebApi({
   clientId: process.env.APP_CLIENT_ID,
@@ -17,26 +31,41 @@ const getOrCreatePlaylist = async (id, title, playlists) => {
 };
 
 const getAll = async (limit, label, func) => {
-  let bar;
-  let fetched = [];
-  let items = [];
-  let offset = 0;
-  let total = 50; // temp total value until first response comes back
-  while (fetched.length < total) {
-    ({ items, total } = await func({ limit, offset }));
-    if (!bar) {
-      bar = new ProgressBar(`fetching ${label} [:bar] :current/:total (:percent) in :elapseds (time remaining :etas)`, {
-        complete: '=',
-        incomplete: '-',
-        width: 20,
-        total
-      });
+  // first request to get total + initialize bar
+  const { total } = await func({ limit });
+  const bar = new ProgressBar(`${label} [:bar] :current/:total (:percent) in :elapseds (time remaining :etas)`, barOpts(total));
+
+  const doFunc = async (args, retries) => {
+    try {
+      const response = await func(args);
+      return response;
+    } catch(e) {
+      if (retries > 0) {
+        bar.interrupt('request failed: ' + e.message + '. retrying!');
+        await asyncTimeout(RETRY_INTERVAL);
+        return doFunc(args, retries - 1);
+      } else {
+        throw e;
+      }
     }
-    offset += items.length;
-    fetched = fetched.concat(items);
-    bar.update(fetched.length / total);
   }
-  return fetched;
+  
+  const requestParams = Array.from(new Array(Math.ceil(total / limit))).map((_, i) => ({
+    limit,
+    offset: i * limit
+  }));
+
+  let totalFetched = 0;
+  return Promise.map(
+    requestParams,
+    async ({ limit, offset }) => {
+      const { items } = await doFunc({ limit, offset }, MAX_RETRIES);
+      totalFetched += items.length;
+      bar.update(totalFetched / total)
+      return items;
+    },
+    { concurrency: 3 }
+  ).then(flatten);
 };
 
 const getAllPlaylists = async (id) =>
@@ -46,7 +75,7 @@ const getAllPlaylists = async (id) =>
   });
 
 const getThisWeeksAlbums = async () => {
-  const albums = getAll(20, 'albums', async ({ limit, offset }) => {
+  const albums = await getAll(20, 'albums', async ({ limit, offset }) => {
     let { body: { albums: { items, total } } } = await spotify.searchAlbums('tag:new', { limit, offset });
     ({ body: { albums: items } } = await spotify.getAlbums(items.map(({ id }) => id)));
     return { items, total }
